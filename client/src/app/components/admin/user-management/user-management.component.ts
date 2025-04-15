7f1cbd75c04037370e5051915fe8a166a36ac815
JamesAskelson <jamesaskelson630@gmail.com>
2025-04-15 15:46:11 -0500

    edit-modal-setup

--- client/src/app/components/admin/user-management/user-management.component.ts
+++ client/src/app/components/admin/user-management/user-management.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { AdminService } from '../../../_services/admin.service';
 import { User } from '../../../_models/user';
+import { RolesModalComponent } from '../../modals/roles-modal/roles-modal.component';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-user-management',
@@ -10,15 +12,34 @@ import { User } from '../../../_models/user';
 })
 export class UserManagementComponent implements OnInit {
   private adminServ = inject(AdminService);
+  private modalServ = inject(BsModalService);
+  private cdr = inject(ChangeDetectorRef);
   users: User[] = []
+  bsModalRef: BsModalRef<RolesModalComponent> = new BsModalRef<RolesModalComponent>();
 
   ngOnInit(): void {
     this.getUsersWithRoles();
+    console.log(this.users)
+  }
+
+  openRolesModal() {
+    const initialState: ModalOptions = {
+      class: "modal-lg",
+      initialState: {
+        title: 'User roles',
+        list: ['Admin', 'Moderator', 'Member']
+      }
+    }
+    this.bsModalRef = this.modalServ.show(RolesModalComponent, initialState)
   }
 
   getUsersWithRoles() {
     this.adminServ.getUserWithRoles().subscribe({
-      next: users => this.users = users
+      next: users => {
+        this.users = users,
+        this.cdr.markForCheck(),
+        console.log(this.users)
+      }
     })
   }
 }
